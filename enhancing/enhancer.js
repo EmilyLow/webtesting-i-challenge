33e1ea5507240aaf84b4b890260e10712fc52bd6
EmilyLow <emlow@me.com>
2020-11-04 16:13:20 -0600

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,36 @@ module.exports = {
   get,
 };
 
+//!! Is creating a new object like this necessary?
 function success(item) {
-  return { ...item };
+  let newItem = {...item};
+  if (item.enhancement < 20) {
+    newItem.enhancement = item.enhancement + 1;
+  }
+  
+  
+  return newItem;
 }
 
+// !!! This is an ugly way to do this
 function fail(item) {
-  return { ...item };
+  let newItem = {...item};
+  if(item.enhancement < 15) {
+    newItem.durability = item.durability - 5;
+  }
+  else {
+    newItem.durability = item.durability - 10;
+  }
+  if(item.enhancement > 16) {
+    newItem.enhancement = item.enhancement -1;
+  }
+  return newItem;
 }
 
 function repair(item) {
-  return { ...item };
+  let newItem = {...item};
+  newItem.durability = 100; 
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
-const enhancer = require('./enhancer.js');
+
 // test away!
+
+const enhancer = require("./enhancer");
+const { describe } = require('yargs');
+const { it, expect, test } = require('@jest/globals');
+
+
+// test("testing works", () => {
+//             expect(1+1).toBe(2)
+//         })
+
+
+///!!! Why does describe not work??? 
+
+// describe("enhancer unit tests", () => {
+//     it("testing works", () => {
+//         expect(1+1).toBe(2)
+//     })
+
+//     it("repairs", () => {
+//         expect(enhancer.repair({name: "one", durability: 0, enhancement: 20 })).toEqual({name: "one", durability: 100, enhancement: 20})
+
+
+// })
+
+test("repair test", () => {
+    expect(enhancer.repair({name: "one", durability: 0, enhancement: 20 })).toEqual({name: "one", durability: 100, enhancement: 20})
+
+    expect(enhancer.repair({name: "two", durability: 100, enhancement: 20 })).toEqual({name: "two", durability: 100, enhancement: 20 })
+
+
+    expect(enhancer.repair({name: "three", durability: 35, enhancement: 20 })).toEqual({name: "three", durability: 100, enhancement: 20 })
+
+})
+
+test("enhancement success test", () => {
+    expect(enhancer.success({name: "one", durability: 0, enhancement: 20 })).toEqual({name: "one", durability: 0, enhancement: 20})
+
+    expect(enhancer.success({name: "two", durability: 50, enhancement: 16 })).toEqual({name: "two", durability: 50, enhancement: 17})
+})
+
+test("enhancement fail test", () => {
+    expect(enhancer.fail({name: "one", durability: 50, enhancement: 10 })).toEqual({name: "one", durability: 45, enhancement: 10})
+
+    expect(enhancer.fail({name: "two", durability: 50, enhancement: 15 })).toEqual({name: "two", durability: 40, enhancement: 15})
+
+    expect(enhancer.fail({name: "three", durability: 50, enhancement: 18 })).toEqual({name: "three", durability: 40, enhancement: 17})
+})
